848c55cecbbf6106e872e5107e317480e4364001
Vitor Hugo Witt <vitorwitt.pro@gmail.com>
2024-12-25 23:33:50 -0300

    feat: :clown_face: Consume mock on request GET

--- src/songs/songs.module.ts
+++ src/songs/songs.module.ts
@@ -2,8 +2,20 @@ import { Module } from '@nestjs/common';
 import { SongsService } from './songs.service';
 import { SongsController } from './songs.controller';
 
+const mockSongService = {
+  findAll() {
+    return [{ id: 1, song: 'Need You by NXTZ' }];
+  },
+};
+
 @Module({
-  providers: [SongsService],
+  providers: [
+    SongsService,
+    {
+      provide: SongsService,
+      useValue: mockSongService,
+    },
+  ],
   controllers: [SongsController],
 })
 export class SongsModule {}
